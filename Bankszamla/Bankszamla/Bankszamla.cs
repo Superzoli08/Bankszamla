efc34afa1d1c593d1b894c1e7a94821107a4ad41
Superzoli08 <b3vetzol@vasvari.org>
2026-03-25 09:29:54 +0100

    Elso mukodo mentes adatok beolvasasa elso metodus

---
+++ Bankszamla/Bankszamla/Bankszamla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankszamla
+{
+    internal class Bankszamla
+    {
+        private string szamlaszam;
+        private string tulajdonos;
+        private decimal egyenleg;
+        private decimal hitelkeret;
+
+        public Bankszamla(string szamlaszam, string tulajdonos, decimal egyenleg)
+        {
+            this.szamlaszam = szamlaszam;
+            this.tulajdonos = tulajdonos;
+            this.egyenleg = egyenleg;
+        }
+
+        public string GetSzamlaszam()
+        {
+            return szamlaszam;
+        }
+
+
+
+        public void Befizetes(List<Bankszamla> adatok, decimal deposit)
+        {
+            
+        }
+    }
+}
---
+++ Bankszamla/Bankszamla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bankszamla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bankszamla> adatok = new List<Bankszamla>();
+            StreamReader sr = new StreamReader("szamlak.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] mezok = sr.ReadLine().Split(';');
+                adatok.Add(new Bankszamla(mezok[0], mezok[1], decimal.Parse(mezok[2])));
+            }
+            sr.Close();
+
+
+        }
+    }
+}
